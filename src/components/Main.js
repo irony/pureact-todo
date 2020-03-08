b75ac2def1dd45d8dd3b06437ebf28bf14e976f0
Christian Landgren <christian@landgren.nu>
2020-03-08 17:48:43 +0100

    tried to convert to parcel

--- src/components/Main.js
+++
@@ -1,9 +0,0 @@
-import React from 'pureact'
-
-const Main = ({children}) => (
-  <section className="main">
-    {children}
-  </section>
-)
-
-export default Main
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React, {render} from 'pureact'
-import App from './components/App'
-import store from './store'
-
-let oldTree
-store.subscribe(() => {
-  const state = store.getState()
-  oldTree = render(<App {...state} />, document.getElementById('root'), oldTree)
-})
